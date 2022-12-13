f40a7bb598a4fc57adabe8690c32f96c2cdcfc22
day <lilitmousheghyan@gmail.com>
2022-12-13 15:08:49 +0200

    initialized window

--- code/src/main.rs
+++ code/src/main.rs
@@ -2,5 +2,21 @@ extern crate sdl2;
 
 fn main() {
     println!("Hello, world!");
-    let _sdl = sdl2::init().unwrap();
+
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem=sdl.video().unwrap();
+    let _window=video_subsystem.window("Ubiland Saga", 800, 600).resizable().build().unwrap();
+
+    let mut event_pump=sdl.event_pump().unwrap();
+    'main : loop{
+        for event in event_pump.poll_iter(){
+            //user input
+            match event {
+                sdl2::event::Event::Quit {..}=>break 'main,
+                _ => {},
+            }
+        }
+
+        //rendering
+    }
 }
