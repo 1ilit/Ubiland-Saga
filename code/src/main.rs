a9990c0bc060faf8a94462af8dca718817c956dc
day <lilitmousheghyan@gmail.com>
2022-12-21 18:02:17 +0200

    brogress

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,45 +1,58 @@
 #[macro_use]
 extern crate glium;
+extern crate image;
 
 fn main() {
     println!("Hello, world!");
     use glium::{glutin, Surface};
 
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
+    let wb = glutin::window::WindowBuilder::new().with_title(format!("hi"));
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     #[derive(Copy, Clone)]
     struct Vertex {
         position: [f32; 2],
+        color: [f32; 4],
     }
 
-    implement_vertex!(Vertex, position);
+    implement_vertex!(Vertex, position, color);
 
     let vertex1 = Vertex {
-        position: [-0.5, -0.5],
+        position: [0.5, -0.5],
+        color: [0.0, 0.0, 1.0, 1.0],
     };
     let vertex2 = Vertex {
-        position: [0.0, 0.5],
+        position: [0.5, 0.5],
+        color: [0.0, 1.0, 0.0, 1.0],
     };
     let vertex3 = Vertex {
-        position: [0.5, -0.25],
+        position: [-0.5, -0.5],
+        color: [1.0, 0.0, 1.0, 1.0],
     };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex4 = Vertex {
+        position: [-0.5, 0.5],
+        color: [0.0, 0.0, 1.0, 1.0],
+    };
+    let shape = vec![vertex1, vertex2, vertex3, vertex4];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TriangleStrip);
 
     let vertex_shader_src = r#"
     #version 140
 
     in vec2 position;
+    in vec4 color; 
+
+    out vec4 ourColor;
 
     uniform mat4 matrix;
 
     void main() {
+        ourColor=color;
         gl_Position = matrix * vec4(position, 0.0, 1.0);
     }
     "#;
@@ -47,10 +60,11 @@ fn main() {
     let fragment_shader_src = r#"
     #version 140
 
-    out vec4 color;
+    out vec4 fragColor;
+    in vec4 ourColor;
 
     void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+        fragColor = ourColor;
     }
     "#;
 
@@ -67,13 +81,12 @@ fn main() {
 
         let uniforms = uniform! {
             matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32],
-            ]
+                [ t , 0.0, 0.0, 1.0f32],
+            ],
         };
-
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
         target
@@ -86,7 +99,6 @@ fn main() {
             )
             .unwrap();
         target.finish().unwrap();
-
 
         let next_frame_time =
             std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
